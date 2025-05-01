df7294c91e45060519af3ebd3ac6397f6b599449
alexhenry123 <2134782@cegepmontpetit.ca>
2025-05-01 19:55:21 -0400

    commit

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('CELA FONCTIONNE ??\n');
 });
-
-
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
