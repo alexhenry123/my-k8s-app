6d2a27549006b3cc99b921c159a86f2db62460fb
alexhenry123 <2134782@cegepmontpetit.ca>
2025-05-02 12:22:48 -0400

    chgmt code qa

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Code du qa !\n');
+    res.end('Code qa\n');
 });
-
 
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
