c026cddd6b1dec48ef760919eb1c28c4bbdaa0be
alexhenry123 <2134782@cegepmontpetit.ca>
2025-05-02 14:25:59 -0400

    chgmt message qa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Code qa\n');
+    res.end('Code qa qui change automatiquement waaaaa\n');
 });
 
 server.listen(3000, () => {
