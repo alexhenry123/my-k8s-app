188dab6dcd104f1c1eb91c7b7bcc7f3165813ad0
alexhenry123 <2134782@cegepmontpetit.ca>
2025-05-02 15:15:11 -0400

    tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Code dev waweeee\n');
+    res.end('Code dev waweeee 2\n');
 });
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
